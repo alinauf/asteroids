8345c3923ce689af4773c540076f513af09547f3
Nauf <naufpsychic@gmail.com>
2025-01-31 19:51:58 +0500

    pygame setup

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from constants import *
+
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()  # display update
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+                # pygame.quit()
+                # sys.exit()
+
+    # print("Starting asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
