7a1369fe14507d37d2ab6e64270f9874d3bc4ec3
Nauf <naufpsychic@gmail.com>
2025-02-01 22:56:16 +0500

    feat(wip): draw player

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pygame
 from constants import *
-
-
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()  # display update
-        
+        clock = pygame.time.Clock()
+        dt = 0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                dt = clock.tick(60)
                 return
                 # pygame.quit()
                 # sys.exit()
