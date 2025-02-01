eb67ecde3fa30dc1a51ba7f673096d782858deba
Nauf <naufpsychic@gmail.com>
2025-02-01 23:04:41 +0500

    feat: rotate player

--- main.py
+++ main.py
@@ -6,16 +6,22 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+                
+        player.update(dt)
         screen.fill((0, 0, 0))
         player.draw(screen)
         pygame.display.flip()  # display update
         clock = pygame.time.Clock()
-        dt = 0
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                dt = clock.tick(60)
-                return
+
+        dt = clock.tick(60)
+
+       
                 # pygame.quit()
                 # sys.exit()
 
